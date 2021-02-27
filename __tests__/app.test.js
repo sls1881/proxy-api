1c87943f4d8a37a383e48d5347e3f4d938acd708
‘Shelby <sls1881@gmail.com>
2021-02-27 14:19:06 -0800

    correcting tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,7 +14,6 @@ describe('app routes', () => {
         'longitude': '-122.6741949',
       };
 
-
       const actual = [
         {
           'place_id': '282983083',
@@ -120,55 +119,55 @@ describe('app routes', () => {
       ];
 
       const actual = {
-        "businesses": [
+        'businesses': [
           {
-            "id": "Ys42wLKqrflqmtqkgqOXgA",
-            "alias": "luc-lac-portland-7",
-            "name": "Luc Lac",
-            "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/azr6sD6VeJbdaiO2aKvSsw/o.jpg",
-            "is_closed": false,
-            "url": "https://www.yelp.com/biz/luc-lac-portland-7?adjust_creative=CQE1JjgHx0oM_A9JTQhg7A&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=CQE1JjgHx0oM_A9JTQhg7A",
-            "review_count": 3205,
-            "categories": [
+            'id': 'Ys42wLKqrflqmtqkgqOXgA',
+            'alias': 'luc-lac-portland-7',
+            'name': 'Luc Lac',
+            'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/azr6sD6VeJbdaiO2aKvSsw/o.jpg',
+            'is_closed': false,
+            'url': 'https://www.yelp.com/biz/luc-lac-portland-7?adjust_creative=CQE1JjgHx0oM_A9JTQhg7A&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=CQE1JjgHx0oM_A9JTQhg7A',
+            'review_count': 3205,
+            'categories': [
               {
-                "alias": "vietnamese",
-                "title": "Vietnamese"
+                'alias': 'vietnamese',
+                'title': 'Vietnamese'
               },
               {
-                "alias": "tapasmallplates",
-                "title": "Tapas/Small Plates"
+                'alias': 'tapasmallplates',
+                'title': 'Tapas/Small Plates'
               },
               {
-                "alias": "cocktailbars",
-                "title": "Cocktail Bars"
+                'alias': 'cocktailbars',
+                'title': 'Cocktail Bars'
               }
             ],
-            "rating": 4.0,
-            "coordinates": {
-              "latitude": 45.516868,
-              "longitude": -122.675447
+            'rating': 4.0,
+            'coordinates': {
+              'latitude': 45.516868,
+              'longitude': -122.675447
             },
-            "transactions": [
-              "pickup",
-              "delivery"
+            'transactions': [
+              'pickup',
+              'delivery'
             ],
-            "price": "$$",
-            "location": {
-              "address1": "835 SW 2nd Ave",
-              "address2": null,
-              "address3": "",
-              "city": "Portland",
-              "zip_code": "97204",
-              "country": "US",
-              "state": "OR",
-              "display_address": [
-                "835 SW 2nd Ave",
-                "Portland, OR 97204"
+            'price': '$$',
+            'location': {
+              'address1': '835 SW 2nd Ave',
+              'address2': null,
+              'address3': '',
+              'city': 'Portland',
+              'zip_code': '97204',
+              'country': 'US',
+              'state': 'OR',
+              'display_address': [
+                '835 SW 2nd Ave',
+                'Portland, OR 97204'
               ]
             },
-            "phone": "+15032220047",
-            "display_phone": "(503) 222-0047",
-            "distance": 1312.1776320869053
+            'phone': '+15032220047',
+            'display_phone': '(503) 222-0047',
+            'distance': 1312.1776320869053
           }
         ]
       };
